5e0681a6afff6859d080a6dd0fb0b72006e6f461
misollae <leticia.isb.lucas@gmail.com>
2021-12-13 14:31:25 +0000

    compo

--- src/Comportamento.java
+++ src/Comportamento.java
@@ -1,15 +1,8 @@
 import javax.swing.JFrame;
 
 public abstract class Comportamento extends Thread {
-//	protected BufferCircular buffer;
 	protected ClienteDoRobot cliente;
-//	protected enum Estado { EM_ESPERA, ESPAÇAR, DESENHAR };
-//	protected Estado estadoAtual;
 	protected JFrame frameCliente;
-	
-//	public Comportamento() {
-//		this.estadoAtual = Estado.EM_ESPERA;
-//	}
 	
 	public void setCliente(ClienteDoRobot cliente) {
 		this.cliente = cliente;
